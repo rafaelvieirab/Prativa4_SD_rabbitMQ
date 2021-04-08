2fb13661281576a4f8e8cee5fc318f7199900a7d
rafaelvieirab <rafaelviebar@gmail.com>
2021-04-08 11:29:03 -0300

    initial commit

---
+++ SpringRabbitMQ/src/test/java/br/ufc/rafael/SpringRabbitMQ/SpringRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufc.rafael.SpringRabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
